50d51d64d0150ff7d388425dd1f19ca92651a409
spaezsuarez <spaezsuarez@gmail.com>
2020-08-24 11:19:35 -0500

    Refactor

--- scripts/claseUno.js
+++
@@ -1,4 +0,0 @@
-var nombre = prompt("Ingresa tu nombre");
-
-console.log("Hola "+ nombre);
-alert("Hola "+ nombre);
--- scripts/desafio1.js
+++
@@ -1,4 +0,0 @@
-alert("Funciona");
-var nombre = prompt("Ingresa tu nombre");
-var lastCaracter = nombre.substr(nombre.length-1);
-alert("El ultimo caracter de tu nombre es: " + lastCaracter);
